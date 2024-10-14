52643cba1160f1b49772efd322381c21a174d89b
salihay <mrsalihay@gmail.com>
2024-10-14 04:11:48 +0300

    Güncelleme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,5 @@
 
-const observer = new PerformanceObserver((list) => {
-  list.getEntries().forEach((entry) => {
-    const request = entry.responseStart - entry.requestStart;
-    if (request > 0) {
-      console.log(`${entry.name}: Request time: ${request}ms`);
-    }
-  });
-});
-
-
-observer.observe({ type: "resource", buffered: true });
-
 (function ($) {
-  
-
-  
-
-  
 
     $(window).scroll(function(){
         var sticky = $('header'),
