98324d3de8faf40648227721f3c51e8ad206a3c4
salihay <mrsalihay@gmail.com>
2024-10-14 04:00:09 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
 
+const observer = new PerformanceObserver((list) => {
+  list.getEntries().forEach((entry) => {
+    const request = entry.responseStart - entry.requestStart;
+    if (request > 0) {
+      console.log(`${entry.name}: Request time: ${request}ms`);
+    }
+  });
+});
 
+observer.observe({ type: "resource", buffered: true });
 
 (function ($) {
   
 
+  
 
   
 
