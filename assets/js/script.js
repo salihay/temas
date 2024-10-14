24424bb554d5bc11a7abfdf9256331e254cdd0f0
salihay <mrsalihay@gmail.com>
2024-10-14 03:41:22 +0300

    Temas
    tms

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+
+
+
+(function ($) {
+  
+
+
+  
+
+    $(window).scroll(function(){
+        var sticky = $('header'),
+            scroll = $(window).scrollTop();
+      
+        if (scroll >= 72) sticky.addClass('scroll');
+        else sticky.removeClass('scroll');
+      });
+      const swiperThumb = new Swiper('.thumbSwiper', {
+        spaceBetween: 0,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+
+    const swiper = new Swiper('.t-swiper', {
+        speed: 400,
+        spaceBetween: 0,
+        effect: "fade",
+        fadeEffect: {
+          crossFade: true
+        },
+          thumbs: {
+            swiper: swiperThumb,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+      });
+      
+     
+
+
+
+
+
+
+
+
+
+})(window.jQuery);
