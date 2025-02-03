334d0b4f359205253520266cc0a62bf25cae552e
salihay <mrsalihay@gmail.com>
2025-02-03 13:06:00 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-(function ($) {
-
+(async function ($) {
 
   let dv = document.querySelectorAll("*")
 
   async function getData() {
     const url = "assets/language/lang.json";
+
     try {
       const response = await fetch(url);
-      console.log(response)
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
@@ -15,10 +14,10 @@
       const json = await response.json();
       var lang = json
 
-
+      var $langSelect = $("#langselect");
       let langSelect = document.querySelector("#langselect")
-
-
+      let langDrop = document.querySelector(".lang_select")
+      let lngSel = document.querySelectorAll(".lang_item")
       if (langSelect.selectedIndex == -1) {
         if (navigator.language == "tr-TR") {
           langSelect.selectedIndex = 0
@@ -29,7 +28,52 @@
 
 
 
+
       let lng = langSelect.value
+
+
+      //lang_select_button
+
+
+
+      if (langDrop.querySelector(`[data-lang='${lng}']`)) {
+
+        langDrop.querySelector(".lang_select_button").innerHTML = langDrop.querySelector(`[data-lang='${lng}']`).innerHTML
+
+
+      }
+
+
+
+      $langSelect.on("change", function (e) {
+
+        const map = new Map(Object.entries(lang));
+
+        map.forEach((item) => {
+          if (item.content.node) {
+            item.content.node.innerHTML = item.content[e.target.value]
+          }
+        });
+     
+        langDrop.querySelector(".lang_select_button").innerHTML = langDrop.querySelector(`[data-lang='${$langSelect[0].value}']`).innerHTML
+      });
+
+
+      lngSel.forEach((sl) => {
+
+        sl.addEventListener("click", (e) => {
+
+          //langDrop.querySelector(".lang_select_button").innerHTML= sl.innerHTML
+
+          
+
+          langSelect.value = sl.dataset.lang
+          $($langSelect).trigger("change")
+
+        });
+
+
+      });
 
       dv.forEach((item) => {
         if (!item.children.length) {
@@ -43,138 +87,184 @@
         }
       });
 
-      var $langSelect = $("#langselect");
-
-      $langSelect.on("change", function (e) {
 
 
-        const map = new Map(Object.entries(lang));
-
-        map.forEach((item) => {
-          if (item.content.node) {
-            item.content.node.innerHTML = item.content[e.target.value]
-          }
-        });
-
-      });
-      langSelect.addEventListener("change", (e) => {
-
-
-        const map = new Map(Object.entries(lang));
-
-        map.forEach((item) => {
-          if (item.content.node) {
-            item.content.node.innerHTML = item.content[e.target.value]
-          }
-        });
-      });
     } catch (error) {
       console.error(error.message);
     }
   }
-
-  getData()
-
+  await getData()
 
   $(window).on("load", function () {
-    console.log("load")
+    
+    let intro=document.querySelector(".intro")
+  
+    intro.classList.add("loaded")
+    setTimeout(() => {
+
+      intro.remove()
+    },  300);
   });
 
 
-
-  let sliderHead = document.querySelectorAll(".sl_head")
-  let sliderText = document.querySelectorAll(".sl_text")
+  let sliderHead = document.querySelectorAll(".sl_headx")
+  let sliderText = document.querySelectorAll(".sl_textx")
   sliderHead.forEach((item) => {
-   
-    const nameList = item.innerText.split("");
-   
-    item.innerText = ""
-    nameList.forEach((t) => {
-     
-      let cc = document.createElement("DIV")
+    let arr = item.innerText.split(" ")
+    let tmpArr = []
+    arr.forEach((t, i) => {
+      tmpArr.push(t)
+      if (i + 1 != arr.length) {
+        tmpArr.push("&nbsp;")
+      }
 
-      let cs = document.createElement("SPAN")
-      cs.innerHTML = t
-      cc.append(cs)
-      item.append(cc)
+
     });
+    item.innerHTML = ""
+    tmpArr.forEach((t) => {
+
+
+      if (t == "&nbsp;") {
+
+
+
+
+        let sp_span = document.createElement("span")
+        sp_span.classList.add("spacing")
+        sp_span.innerHTML = t
+        item.append(sp_span)
+      } else {
+        let word_block = document.createElement("div")
+        word_block.classList.add("word_block")
+        t.split("").forEach((tt) => {
+          word_span = document.createElement("span")
+          word_span.classList.add("hide")
+          word_span.innerText = tt
+          word_block.append(word_span)
+        });
+        item.append(word_block)
+
+
+      }
+
+
+
+    });
+
 
 
   });
   sliderText.forEach((item) => {
-    
-    const nameList = item.innerText.split("");
- 
-    item.innerText = ""
-    nameList.forEach((t) => {
-     
-      let cc = document.createElement("DIV")
-      cc.classList.add("hide")
-      let cs = document.createElement("SPAN")
-      cs.innerHTML = t
-      cc.append(cs)
-      item.append(cc)
+
+    let arr = item.innerText.split(" ")
+
+    let tmpArr = []
+    arr.forEach((t, i) => {
+
+
+      tmpArr.push(t)
+      if (i + 1 != arr.length) {
+        tmpArr.push("&nbsp;")
+      }
     });
+
+    item.innerHTML = ""
+    tmpArr.forEach((t) => {
+
+
+      if (t == "&nbsp;") {
+
+
+
+
+        let sp_span = document.createElement("span")
+        sp_span.classList.add("spacing")
+        sp_span.innerHTML = t
+        item.append(sp_span)
+      } else {
+        let word_block = document.createElement("div")
+        word_block.classList.add("word_block")
+        t.split("").forEach((tt) => {
+          word_span = document.createElement("span")
+          word_span.classList.add("hide")
+          word_span.innerText = tt
+          word_block.append(word_span)
+        });
+        item.append(word_block)
+
+
+      }
+
+
+
+    });
+
   });
 
 
 
 
 
-  
 
-  var setAnimation=function(a){
-   
-   
-    let chldhead=sliderHead[a].querySelectorAll("div")
-    let chldtext=sliderText[a].querySelectorAll("div")
-    let timerH=0
-    let timerT=0
-    let steps=3
-    chldhead.forEach((item) => {
+
+  var setAnimation = function (a) {
+
+
+    let chldhead = sliderHead[a].querySelectorAll("span:not(.spacing)")
+    let chldtext = sliderText[a].querySelectorAll("span:not(.spacing)")
+
+    chldhead.forEach((item, idx) => {
       setTimeout(() => {
-        
+
         item.classList.remove("hide")
-      }, timerH);
-  
-      timerH+=steps
+      }, idx * 20);
+
+
+    });
+
+    chldtext.forEach((item, idx) => {
+      setTimeout(() => {
+
+        item.classList.remove("hide")
+      }, idx * 3);
+
+
+    });
+  }
+  var removeAnimation = function (a) {
+
+
+
+    let chldhead = sliderHead[a].querySelectorAll("span:not(.spacing)")
+    let chldtext = sliderText[a].querySelectorAll("span:not(.spacing)")
+
+    chldhead.forEach((item) => {
+
+
+      item.classList.add("hide")
+
+
+
     });
 
     chldtext.forEach((item) => {
-      setTimeout(() => {
-        
-        item.classList.remove("hide")
-      }, timerT);
-  
-      timerT+=steps
-    });
-  }
-  var removeAnimation=function(a){
-   
-   
 
-    let chldhead=sliderHead[a].querySelectorAll("div")
-    let chldtext=sliderText[a].querySelectorAll("div")
-   
-    chldhead.forEach((item) => {
-    
-        
-        item.classList.add("hide")
-   
-  
-    
-    });
 
-    chldtext.forEach((item) => {
-     
-        
-        item.classList.add("hide")
-   
-  
-   
+      item.classList.add("hide")
+
+
+
     });
 
   }
+
+
+
+
+
+
+
+
   $(window).scroll(function () {
     var sticky = $('header'),
       scroll = $(window).scrollTop();
@@ -205,8 +295,8 @@
     },
     on: {
       init: function (e) {
-       
-       
+
+
         //setAnimation(e.activeIndex)
       }
     }
@@ -215,75 +305,11 @@
   swiper.on('slideChange', function (e) {
     //setAnimation(e.activeIndex)
     //removeAnimation(e.previousIndex)
-    
-  });
- 
-  
-  $('select').each(function () {
-    let _this = this
-    function _setIcon_Select(data) {
-      if (!data.id) {
-        if (_this.dataset.placeicon) {
-          var _text = data.text
-          var _p_icon = _this.dataset.placeicon
-          var $place_data = $(`<div class="select-icon">${_p_icon}<span class="_text">${_text}</span></div>`); return $place_data
-        } else if (_this.dataset.image) {
-          var _text = data.text
-          var _p_img = _this.dataset.image
-          var $place_data = $(`<div class="select-icon"><span class="_img"><img src="${_p_img}"></span><span class="_text">${_text}</span></div>`); return $place_data
-        } else if (_this.dataset.placeicon_static) {
-          var _text = data.text
-          var _p_icon_s = _this.dataset.placeicon_static
-          var $place_data = $(`<div class="select-icon">${_p_icon_s}<span class="_text">${_text}</span></div>`); return $place_data
-        } else { return data.text; }
-      }
-      if (_this.dataset.placeicon_static) {
-        var _text = data.text
-        var _p_icon_s = _this.dataset.placeicon_static
-        var $place_data = $(`<div class="select-icon">${_p_icon_s}<span class="_text">${_text}</span></div>`); return $place_data
-      }
-      if (data.element.dataset.icon) {
-        let _icon = data.element.dataset.icon
-        var $data = $(`<div class="select-icon">${_icon}<span class="_text">${data.text}</span></div>`); return $data;
-      } else if (data.element.dataset.image) {
-        let _img = data.element.dataset.image
-        var $data
-        if (data.element.dataset.subText) { $data = $(`<div class="select-icon"><span class="_img"><img src="${_img}"></span><div class="_text_group"><span class="_text">${data.text}</span><span class="s_text">${data.element.dataset.subText}</span></div></div>`); } else { $data = $(`<div class="select-icon"><span class="_img"><img src="${_img}"></span><span class="_text">${data.text}</span></div>`); }
-        return $data;
-      } else { return data.text; }
-      if (_this.multiple) { }
-    }
-    function _setIcon_Result(data) {
-      if (!data.id) {
-        if (_this.dataset.placeicon) {
-          var _text = data.text
-          var _p_icon = _this.dataset.placeicon
-          var $place_data = $(`<div class="select-icon">${_p_icon}<span class="_text">${_text}</span></div>`); return $place_data
-        } else if (_this.dataset.image) {
-          var _text = data.text
-          var _p_img = _this.dataset.image
-          var $place_data = $(`<div class="select-icon"><span class="_img"><img src="${_p_img}"></span><span class="_text">${_text}</span></div>`); return $place_data
-        } else { return data.text; }
-      }
-      if (!data.element) { return data.text; }
-      if (data.element.dataset.icon) {
-        let _icon = data.element.dataset.icon
-        var $data = $(`<div class="select-icon">${_icon}<span class="_text">${data.text}</span></div>`); return $data;
-      } else if (data.element.dataset.image) {
-        let _img = data.element.dataset.image
-        var $data
-        if (data.element.dataset.subText) { $data = $(`<div class="select-icon"><span class="_img"><img src="${_img}"></span><div class="_text_group"><span class="_text">${data.text}</span><span class="s_text">${data.element.dataset.subText}</span></div></div>`); } else { $data = $(`<div class="select-icon"><span class="_img"><img src="${_img}"></span><span class="_text">${data.text}</span></div>`); }
-        return $data;
-      } else { return data.text; }
-      if (_this.multiple) { }
-    }
-    let _search; _this.hasAttribute("search") ? _search = true : _search = false
-    let _type; _this.classList.contains("big") ? _type = true : _type = false
-    let drop__Parent; let drop__Parent__Modal = _this.closest(".modal")
-    _this.closest(".modal") ? drop__Parent = true : drop__Parent = false
-    $(this).select2({ minimumResultsForSearch: _search ? 0 : Infinity, language: "tr", templateResult: _setIcon_Result, templateSelection: _setIcon_Select, theme: "uniselect", dropdownCssClass: _type ? "dr__big" : [], minimumInputLength: _search ? 2 : 0, dropdownParent: drop__Parent ? $(drop__Parent__Modal) : $(document.body), }); if (this.multiple) { $(this).select2({ templateResult: _setIcon_Result, templateSelection: _setIcon_Select, language: "tr", theme: "uniselect", minimumResultsForSearch: _search ? Infinity : Infinity, dropdownCssClass: _type ? "b_m__list" : "m__list", }); }
 
   });
+
+
+
 
 
 
